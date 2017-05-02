263c240f932708cc342f43c3bdf2f55798819a2c
omidleet <omid-noor@hotmail.com>
2017-05-02 21:42:35 +0200

    pushing for heroku

--- serverexample/server.js
+++ serverexample/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 var aboutPost = "Posted by Omid";
 
 app.get('/', function(request, response) {
