a7ef87c55accd1e897062518fc82b8177b4a6aee
omidleet <omid-noor@hotmail.com>
2017-05-03 12:04:56 +0200

    updated app

--- server/server.js
+++ server/server.js
@@ -1,32 +1,52 @@
 var express = require('express');
 var app = express();
-var PORT = 3000;
+var port = process.env.PORT || 3000;
 var aboutPost = "Posted by Omid";
 
 app.get('/', function(request, response) {
     response.send('Hello World!');
-})
+});
 
 app.get('/about', function(request, response) {
     response.send('Written by Omid');
-})
+});
+
+app.get('/json', function(request, response) {
+    response.json({
+        'some_name': 'Value',
+        'an_array_of_objects': [
+            {
+                'another_name': 'Another value',
+                'a_further_name': "A further value"
+            },
+            {'yet_another_name': 'Yet another value'}
+        ],
+        'some_boolean': true,
+        'some_integer': 42,
+        'array_of_ints': [
+            2, 3, 5, 7, 11, 13
+        ],
+        'array_of_strings': [
+            "twee", "drie", "vijf", "zeven"]
+    })
+});
 
 app.post('/about', function(request, response) {
     response.send(aboutPost);
-})
+});
 
 app.post('/', function(request, response) {
     response.send('Hello Avans, POST request received!');
-})
+});
 app.put('/', function(request, response) {
     response.send('Hello Avans, PUT request received!');
-})
+});
 
 app.all('*', function(request, response) {
     response.status(404);
     response.send('404 - Not found');
-})
+});
 
-app.listen(PORT, function() {
-    console.log('Server app is listening on port ' + PORT + '.');
-})
+app.listen(port, function() {
+    console.log('Server app is listening on port ' + port + '.');
+});
